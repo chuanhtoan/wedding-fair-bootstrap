22e701edc97976d315849cc1c24797dc149a5790
chuanhtoan <chuanhtoan139@gmail.com>
2021-03-05 16:12:20 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+window.onscroll = function() {scrollFunction()}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.querySelector(".header__info--fixed").classList.add('disable')
+        document.querySelector(".header__info--sticky").classList.remove('disable')
+
+        document.querySelector(".header__info--hamburger-fixed").classList.add('disable')
+        document.querySelector(".header__info--hamburger-sticky").classList.remove('disable')
+    } else {
+        document.querySelector(".header__info--sticky").classList.add('disable')
+        document.querySelector(".header__info--fixed").classList.remove('disable')
+
+        document.querySelector(".header__info--hamburger-sticky").classList.add('disable')
+        document.querySelector(".header__info--hamburger-fixed").classList.remove('disable')
+    }
+}
+
+function hamburger(x) {
+    if (x.classList.contains("change")) {
+        document.querySelectorAll('.hamburger__container').forEach(hamburger => {hamburger.classList.remove("change")})
+        document.querySelectorAll('#fixed-menu').forEach(menu => {menu.classList.add('hidden')})
+        document.querySelector("#overlay").classList.add('hidden')
+    } else {
+        document.querySelectorAll('.hamburger__container').forEach(hamburger => {hamburger.classList.add("change")})
+        document.querySelectorAll('#fixed-menu').forEach(menu => {menu.classList.remove('hidden')})
+        document.querySelector("#overlay").classList.remove('hidden')
+    }
+}
